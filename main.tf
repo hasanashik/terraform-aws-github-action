c6b856cbb336de60cc9afd28bf2ef69d4d411078
hasanashik <hasanashik100@gmail.com>
2024-04-27 19:50:50 +0600

    updated tf

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.32.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
 }
+
+# terraform {
+#   backend "s3" {
+#     bucket = "zaman-bucket-github"
+#     key    = "default-infrastructure"
+#     region = "us-east-1"
+#   }
+# }
 
 resource "aws_s3_bucket" "zaman-bucket" {
   bucket = "tf-zaman-bucket"
@@ -10,36 +25,5 @@ resource "aws_s3_bucket" "zaman-bucket" {
   tags = {
     Name        = "zaman-bucket"
     Environment = "Dev"
-  }
-}
-resource "aws_vpc" "zaman_vpc" {
-  cidr_block = "10.10.0.0/16"
-  tags = {
-    Name = "zaman_vpc_dev"
-  }
-}
-resource "aws_internet_gateway" "zaman_vpc_igw" {
-  tags = {
-    Name = "zaman_vpc_dev_igw"
-  }
-}
-resource "aws_internet_gateway_attachment" "vpc_igw" {
-  vpc_id              = resource.aws_vpc.zaman_vpc.id
-  internet_gateway_id = resource.aws_internet_gateway.zaman_vpc_igw.id
-
-}
-resource "aws_subnet" "eks_cluster" {
-  vpc_id     = resource.aws_vpc.zaman_vpc.id
-  cidr_block = "10.10.1.0/24"
-
-  tags = {
-    Name = "eks_cluster"
-  }
-}
-resource "aws_subnet" "db_subnet" {
-  vpc_id     = resource.aws_vpc.zaman_vpc.id
-  cidr_block = "10.10.2.0/24"
-  tags = {
-    Name = "eks_cluster_db"
   }
 }
